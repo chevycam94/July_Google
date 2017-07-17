529ba2811d20aa2e6644b09157b1e79b34c35984
Noah Greene <chevycam94@gmail.com>
2017-07-17 16:22:04 -0400

    Revert "patch-3.18.59-60"
    This reverts commit 41009ae179f9d2f989b6f77f5f3900b239e9befb.

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4853,8 +4853,6 @@ static bool emulator_get_segment(struct x86_emulate_ctxt *ctxt, u16 *selector,
 
 	if (var.unusable) {
 		memset(desc, 0, sizeof(*desc));
-		if (base3)
-			*base3 = 0;
 		return false;
 	}
 
