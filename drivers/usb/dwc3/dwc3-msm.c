b3a3403b50d57b005087a774d805afebace3222d
Noah Greene <chevycam94@gmail.com>
2017-07-17 16:21:37 -0400

    Revert "usb: dwc3: Add check for SDP charger type"
    This reverts commit ed3e5b7a14f3df669dfc9a7d47c7827af374c0d0.

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3791,8 +3791,7 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 				pm_runtime_get_noresume(mdwc->dev);
 				dwc3_initialize(mdwc);
 				/* check dp/dm for SDP & runtime_put if !SDP */
-				if (mdwc->detect_dpdm_floating &&
-					mdwc->chg_type == DWC3_SDP_CHARGER) {
+				if (mdwc->detect_dpdm_floating) {
 					dwc3_check_float_lines(mdwc);
 					if (mdwc->chg_type != DWC3_SDP_CHARGER)
 						break;
